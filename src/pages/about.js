1639d73c570360c4cd27f794407738d558452445
André Koga <social@koga.one>
2022-05-11 22:13:15 -0300

    Some clean up

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,7 +7,16 @@ const About = () => {
   return (
     <Layout pageTitle="About">
       <Header title="About"></Header>
-      <Form></Form>
+      <article>
+        <div cvlas>
+          <h2 className="text-d1">ok, so you want to know more about me?</h2>
+        </div>
+        <div className="prose mx-auto mb-4 flex max-w-[80ch] rounded-lg bg-aka bg-opacity-10 px-4 py-8 text-katsu desktop:prose-lg desktop:mb-1 desktop:p-8">
+          <div className="mx-auto">
+            <Form></Form>
+          </div>
+        </div>
+      </article>
     </Layout>
   );
 };
